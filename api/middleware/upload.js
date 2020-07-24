4166d6c1b9e574cc537f9728f026a682b650a338
JunoNgx <juno.ngx@gmail.com>
2020-07-24 10:46:37 +0800

    Fix a typo in GCS credentials

--- api/middleware/upload.js
+++ api/middleware/upload.js
@@ -13,7 +13,7 @@ const storage = new Storage({
         "auth_uri": "https://accounts.google.com/o/oauth2/auth",
         "token_uri": "https://oauth2.googleapis.com/token",
         "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-        "client_x509_cert_url": process.env.GC_SERVICE_ACCOUNT_KEY
+        "client_x509_cert_url": process.env.GC_CLIENT_X509_CERT_URL
       }
 })
 const bucket = storage.bucket(process.env.GC_BUCKET)
